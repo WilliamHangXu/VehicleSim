8514ef38f0d6b570f0888dd69268bdb080d38689
William_HangXu <willxh.68209@gmail.com>
2023-04-09 17:21:58 -0500

    Perception

--- src/perception.jl
+++ src/perception.jl
@@ -6,7 +6,7 @@ cameras.
 Outline of EKF:
     a. Previous measurement: P(xₖ | xₖ₋₁) = 𝒩(μₖ₋₁, Σₖ₋₁)
     b. Process model: P(xₖ | xₖ₋₁) = 𝒩(f(xₖ₋₁), Σₚ)
-    c. Measurement model: P(zₖ | xₖ) = 𝒩(h(xₖ, xegoₖ), Σₘ)
+    c. Measurement model: P(zₖ | xₖ) = 𝒩(h(xₖ, x_egoₖ), Σₘ)
     where x = [p1, p2, θ, v, l, w, h] describes the state of an object, 
     and z = [y1, y2, y3, y4, y5, y6, y7, y8] describes the measurement (bb) collected by the two 
     cameras.
@@ -14,7 +14,7 @@ Outline of EKF:
 """
 
 """
-# Modified from get_body_transform in measurement.jl by Gavin
+# Modified from get_body_transform in measurement.jl by Gavin and William
 This function calculates a matrix that expresses a point in a loc-centered frame into world frame.
 
 @param
@@ -22,8 +22,7 @@ loc: the coordinate of a point.
 @output
 a matrix.
 """
-function get_body_transform_perception(loc)
-    R = one(RotMatrix{3, Float64})
+function get_body_transform_perception(loc, R=one(RotMatrix{3, Float64}))
     [R loc]
 end
 
@@ -63,7 +62,7 @@ function convert_to_pixel(num_pixels, pixel_len, px)
 end
 
 """
-This function predicts current state based on previous state.
+This function predicts current state based on previous state. Created by William
 
 @param
 x: previous state [p1, p2, θ, v, l, w, h]
@@ -85,8 +84,8 @@ function f(x, Δ)
 end
 
 """
-# Modified from cameras in measurement.jl by William Xu
-This function predicts bounding box measurements based on the state of the object being tracked.
+# Modified from cameras in measurement.jl by Gavin and William
+This function predicts bounding box measurements based on the state of the object x being tracked.
 
 @param
 x: the state of the object being tracked
@@ -96,77 +95,55 @@ x_ego: the state of ego vehicle
 bounding box measurement of the object being tracked
 """
 function h(x, x_ego, focal_len=0.01, pixel_len=0.001, image_width=640, image_height=480)
-    # the position of the vehicle in world frame
+    # the position of the ego vehicle in world frame
     x_ego_pos_world = x_ego[1:3]      # p1, p2, p3
     # the position of the object in world frame
     x_pos_world = x[1:3]              # p1, p2, p3
-    # the angle of the vehicle
-    x_ego_angles = x_ego[4:6]   # r, p, y
-    x_size = x[6:8]             # l, w, h
-    
-    # two cameras
-    z1 = []
-    z2 = []
+    # the angle of the ego vehicle
+    x_ego_angles = x_ego[4:6]         # r, p, y
+    # the size of object
+    x_size = x[6:8]                   # l, w, h
+    # this stores the 2d bounding box coordinates
+    z = []
 
     # corners_world is an array that contains coordinates of 3d bbox of the object in world frame
     corners_world = get_3d_bbox_corners_perception(x_pos_world, x_size)
-    corners_img_1 = []
-    corners_img_2 = []
 
+    # initialize the boundaries of the 2d bbox
     left = image_width/2
     right = -image_width/2
     top = image_height/2
     bot = -image_height/2
 
-    # This for-loop convert coordinates in corners_world into coordinates expressed in two 
-    # cameras' image frames.
-    for world in corners_world
+    # This part takes care of the camera's angle and its relative position w.r.t. ego.
+    T_body_cam1 = get_cam_transform(1)
+    T_body_cam2 = get_cam_transform(2)
 
-        # x_pos_ego is the coordinate of a point (one of the corners of bbox) expressed in ego frame     
-        x_pos_ego = world - x_ego_pos_world
+    # This part changes the camera frame into the rotated camera frame, where z points forward.
+    T_cam_camrot = get_rotated_camera_transform()
 
-        # x_ego_rot is a matrix that can correct ego's rpy angles.
-        # Now rpy should be 0 for the ego car.
-        x_ego_rot = inv(RotXYZ(x_ego_angles))
+    # This part combines the two transformations together.
+    T_body_camrot1 = multiply_transforms(T_body_cam1, T_cam_camrot)
+    T_body_camrot2 = multiply_transforms(T_body_cam2, T_cam_camrot)
 
-        # Since we corrected all the rpy angles, we must correct the position of the object as well, 
-        # so its relatve position to the camera doesn't change.
-        # After applying x_ego_rot, the new x_pos_body is now the corrected coordinate of a corner.
-        x_pos_ego = x_ego_rot * x_pos_ego
+    # This part takes care of ego's rpy angles and the object's relative position w.r.t. ego.
+    T_world_body = get_body_transform_perception(x_ego_pos_world, RotXYZ(x_ego_angles))
 
-        # express the corner in camera frame
-        x_pos_camera_1 = x_pos_ego - [1.35, 1.7, 2.4]
-        x_pos_camera_2 = x_pos_ego - [1.35, -1.7, 2.4]
+    # Combines all the transformations together (order matters)
+    T_world_camrot1 = multiply_transforms(T_world_body, T_body_camrot1) 
+    T_world_camrot2 = multiply_transforms(T_world_body, T_body_camrot2)
 
-        # correct the camera angle
-        cam_rot = inv(RotY(0.02))
-        x_pos_camera_1 = cam_rot * x_pos_camera_1
-        x_pos_camera_2 = cam_rot * x_pos_camera_2
+    # We need to invert the transformations above to get the correct order.
+    # change to ego frame -> adjust rpy angle -> change to camera frame -> adjust cam angle -> change to rotated camera frame
+    T_camrot1_world = invert_transform(T_world_camrot1)
+    T_camrot2_world = invert_transform(T_world_camrot2)
 
-        # switch to rotated camera frame
-        R = [0 0 1.;
-            -1 0 0;
-            0 -1 0]
-        x_pos_camera_1 = R * x_pos_camera_1
-        x_pos_camera_2 = R * x_pos_camera_2
+    for transform in (T_camrot1_world, T_camrot2_world)
+        # apply the transformation to points in corners_world to convert them into rotated camera frame
+        vehicle_corners = [transform * [pt;1] for pt in corners_world]
 
-        if (x_pos_camera_1[3] < focal_len || x_pos_camera_2[3] < focal_len)
-            break
-        end
-
-        # switch to image frame (y1, y2)
-        x_pos_img_1 = [x_pos_camera_1[1]/x_pos_camera_1[3] * focal_len, x_pos_camera_1[2]/x_pos_camera_1[3] * focal_len]
-        x_pos_img_2 = [x_pos_camera_2[1]/x_pos_camera_2[3] * focal_len, x_pos_camera_2[2]/x_pos_camera_2[3] * focal_len]
-        
-        # store the result into two arrays
-        push!(corners_img_1, x_pos_img_1)
-        push!(corners_img_2, x_pos_img_2)
-    end
-
-    # convert 3d bounding box (now in image frame) into 2d bounding box.
-    for (corners, cam) in zip((corners_img_1, corners_img_2), (z1, z2))
-        # find the top, left, bottom, right points.
-        for corner in corners
+        # convert the points from rotated camera frame into image frame (y1, y2)
+        for corner in vehicle_corners
             if corner[3] < focal_len
                 break
             end
@@ -177,19 +154,15 @@ function h(x, x_ego, focal_len=0.01, pixel_len=0.001, image_width=640, image_hei
             top = min(top, py)
             bot = max(bot, py)
         end
-        # convert these four points into pixel position.
-        if top ≈ bot || left ≈ right || top > bot || left > right
-            # out of frame
-            continue
-        else 
-            top = convert_to_pixel(image_height, pixel_len, top)
-            bot = convert_to_pixel(image_height, pixel_len, bot)
-            left = convert_to_pixel(image_width, pixel_len, left)
-            top = convert_to_pixel(image_width, pixel_len, right)
-            push!(cam, SVector(top, left, bot, right))
-        end
+
+        # convert image frame coordinates into pixel numbers
+        top = convert_to_pixel(image_height, pixel_len, top) # top 0.00924121388699952 => 251
+        bot = convert_to_pixel(image_height, pixel_len, bot)
+        left = convert_to_pixel(image_width, pixel_len, left)
+        top = convert_to_pixel(image_width, pixel_len, right)
+
+        push!(z, SVector(top, left, bot, right))
     end
-    # z1 and z2 contain 2d bbox for each camera respectively.
-    [z1 z2]
+    z
 end
 
